fab0a561b252021c4dbf35b6ca4e42de6ee4d16d
neokoni <dulin081682@126.com>
2025-06-02 14:28:19 +0800

    chore: remove useless imports

--- src/main/java/ink/neokoni/lightchainbreak/utils/enchantments.java
+++ src/main/java/ink/neokoni/lightchainbreak/utils/enchantments.java
@@ -1,11 +1,7 @@
 package ink.neokoni.lightchainbreak.utils;
 
-import org.bukkit.block.Block;
 import org.bukkit.enchantments.Enchantment;
-import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
-import org.bukkit.inventory.meta.Damageable;
-import org.bukkit.inventory.meta.ItemMeta;
 
 public class enchantments {
     public static boolean hasUnbreak(ItemStack i){
